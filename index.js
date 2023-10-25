263adfbff8e6fe276c96ada820927c12b100268c
Kanhaiya Gawali <124244655+kanhaiya9309@users.noreply.github.com>
2023-10-26 00:55:32 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,219 @@
+//alert("Hello");
+
+const searchButton =document.querySelector(".serch-btn");
+
+const locationButton =document.querySelector(".location-btn");
+
+const cityInput =document.querySelector(".city-input");
+
+const API_KEY = "4cebbc0f439bb72273911083380123e7" ;
+
+const weatherCardsDiv= document.querySelector(".weather-cards");
+
+const currentWeatherDiv= document.querySelector(".current-weather");
+
+
+
+
+
+
+
+const createWeatherCard = (cityName,weatherItem,index) => {
+
+    if (index === 0) {
+
+        return `
+
+          <div class="details">
+
+            <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+
+            <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+
+            <h4>Wind: ${weatherItem.wind.speed} m/s</h4>
+
+            <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+
+          </div>
+
+          <div class="icon">
+
+            <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="Weather Img">
+
+            <h4>${weatherItem.weather[0].description}</h4>
+
+          </div>
+
+        `;
+
+      } else {
+
+        return `
+
+          <li class="card">
+
+            <h2>${weatherItem.dt_txt.split(" ")[0]}</h2>
+
+            <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="Weather Img">
+
+            <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+
+            <h4>Wind: ${weatherItem.wind.speed} m/s</h4>
+
+            <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+
+          </li>
+
+        `;
+
+      }
+
+      
+
+  }
+
+  
+
+
+
+const getWeatherDetails =(cityName,lat ,lon) =>{
+
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+
+
+    fetch(WEATHER_API_URL).then(res=>res.json()).then(data=>{
+
+        const uniqueForcastDays=[];
+
+        const fiveDaysForcast = data.list.filter(forcast=>{
+
+            const forecastDate =new Date (forcast.dt_txt).getDate();
+
+          if(!uniqueForcastDays.includes(forecastDate)){
+
+             return uniqueForcastDays.push(forecastDate);
+
+          }
+
+        })
+
+      
+
+        cityInput.value = "";
+
+        currentWeatherDiv.innerHTML = "";
+
+        weatherCardsDiv.innerHTML = "";
+
+
+
+        fiveDaysForcast.forEach((weatherItem,index) =>{
+
+            if(index===0){
+
+                currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName,weatherItem,index));
+
+            } else{
+
+                weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard( cityName,weatherItem,index));
+
+            }
+
+        }) 
+
+    }).catch(()=>{
+
+        alert("An error occured whie fetching the weather forcast!")
+
+    })
+
+}
+
+
+
+
+
+const getCityCoordinates =()=>{
+
+     const cityName = cityInput.value.trim();
+
+     if(!cityName) return;
+
+    const GEOCODING_API_URL =`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`
+
+
+
+    fetch(GEOCODING_API_URL).then(res=>res.json()).then(data=>{
+
+      if(!data.length) return alert( `No Coordinates found for ${cityName}`);
+
+      const {name,lat,lon} =data[0];
+
+      getWeatherDetails(name,lat,lon);
+
+
+
+    }).catch(()=>{
+
+        alert("An error occured while fetching the coordinates!");
+
+    })
+
+
+
+}
+
+const getUserCoordinates =()=>{
+
+   navigator.geolocation.getCurrentPosition(
+
+    position =>{
+
+       const{latitude ,longitude} =position.coords;
+
+       const REVERSE_GEOCODING_URL =`https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`
+
+       fetch(REVERSE_GEOCODING_URL).then(res=>res.json()).then(data=>{
+
+        const {name} =data[0];
+
+        getWeatherDetails(name,latitude,longitude);
+
+
+
+      }).catch(()=>{
+
+          alert("An error occured while fetching the City!");
+
+      })
+
+    },
+
+    error =>{
+
+       if(error.code === error.PERMISSION_DENIED){
+
+        alert("Geolacation request denied . Please reset location permission to grant access again");
+
+       }
+
+    }
+
+   )
+
+}
+
+
+
+
+
+searchButton.addEventListener("click",getCityCoordinates);
+
+locationButton.addEventListener("click",getUserCoordinates);
+
+cityInput.addEventListener("keyup",e=>e.key ==="Enter" && getCityCoordinates())
+
+
+
+
